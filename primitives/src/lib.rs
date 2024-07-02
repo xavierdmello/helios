a0c890d717a91c26fa2c83d3ebc5b108966d2600
Xavier DMello <xaviermdmello@gmail.com>
2024-07-01 17:13:03 -0700

    ye

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -1,11 +1,23 @@
-pub mod consensus;
+
 pub mod errors;
-pub mod forktypes;
-pub mod types;
+
 pub mod utils;
 
 pub mod execution {
     pub mod constants;
     pub mod errors;
+    pub mod types;
+}
+
+pub mod common {
+    pub mod errors;
+    pub mod types;
+}
+
+pub mod config {
+    pub mod types;
+}
+
+pub mod consensus {
     pub mod types;
 }
